26cf8d580034b9f59b191aa2738ad30d346ae79b
Allaith Ahmed Alzoubi <allaith.alzoubi@mail.utoronto.ca>
2025-05-07 02:11:48 -0400

    Final Project

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -1,15 +1,17 @@
 import os
+from dotenv import load_dotenv
 
-COHERE_API_KEY = '86GngtGqjEgRBd8njQkXQ9fl4LcRWY9dhpwY1vvs'
+load_dotenv()
+COHERE_API_KEY = os.getenv('COHERE_API_KEY')
 COHERE_API_URL = 'https://api.cohere.ai/v1/generate'
 
 
 SUPABASE_URL = 'https://voenczphlgojgihwbcwi.supabase.co'
-SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvZW5jenBobGdvamdpaHdiY3dpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk2MDM0NzgsImV4cCI6MjA1NTE3OTQ3OH0.WASlZSz_mSEyxlDZxDhUWZOdQp9JG0n7IHvE0Y8Mo6Y'
+SUPABASE_KEY = os.getenv('SUPABASE_KEY')
 
 
-LINKEDIN_CLIENT_ID = '778z82h4dtgrrz'
-LINKEDIN_CLIENT_SECRET = 'WPL_AP1.vFgMgRskVtKfk2hP.RcATFw=='
+LINKEDIN_CLIENT_ID = os.getenv('LINKEDIN_CLIENT_ID')
+LINKEDIN_CLIENT_SECRET = os.getenv('LINKEDIN_CLIENT_SECRET')
 REDIRECT_URI = 'http%3A%2F%2F127.0.0.1%3A5000%2Flinkedin-openid%2Fcallback'
 BACKEND_REDIRECT_URI = 'http://127.0.0.1:5000/linkedin-openid/callback'
 FRONTEND_REDIRECT_URI = 'http://localhost:3000/resumeupload'
